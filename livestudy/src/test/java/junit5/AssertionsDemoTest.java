2e2968f562dd184eae440ff059c0ff4fc23fc3a1
HiiWee <wpdlzhf159@gmail.com>
2022-07-14 23:26:40 +0900

    Comment: change English comment to Korean

--- livestudy/src/test/java/junit5/AssertionsDemoTest.java
+++ livestudy/src/test/java/junit5/AssertionsDemoTest.java
@@ -25,14 +25,13 @@ class AssertionsDemoTest {
         assertEquals(2, calculator.add(1, 1));
         assertEquals(4, calculator.multiply(2, 2),
                 "하나의 옵션인 추가 메시지는 마지막 파라미터에 넣음");
-        assertTrue('a' < 'b', () -> "Assertion messages can be lazily evaluated -- "
-                + "to avoid constructing complex messages unnecessarily.");
+        assertTrue('a' < 'b', () -> "Assertion messages는 지연로딩과 같이 동작 하여 -- "
+                + "복잡한 메시지를 불필요하게 구성하지 않도록 한다.");
     }
 
     @Test
     void groupedAssertions() {
-        // In a grouped assertion all assertions are executed, and all
-        // failures will be reported together.
+        // 그룹 assertion에서 모든 assertions가 실행되고 모든 실패가 같이 보고됩니다.
         assertAll("person",
                 () -> assertEquals("Jane", person.getFirstName()),
                 () -> assertEquals("Doe", person.getLastName())
@@ -41,26 +40,24 @@ class AssertionsDemoTest {
 
     @Test
     void dependentAssertions() {
-        // Within a code block, if an assertion fails the
-        // subsequent code in the same block will be skipped.
+        // 코드 블록내에서 특정 assertion이 실패하면 동일 블록에서 후속 코드는 생략합니다.
         assertAll("properties",
                 () -> {
                     String firstName = person.getFirstName();
                     assertNotNull(firstName);
 
-                    // Executed only if the previous assertion is valid.
+                    // assertNotNull()이 검증 됐을경우에만 실행됨
                     assertAll("first name",
                             () -> assertTrue(firstName.startsWith("J")),
                             () -> assertTrue(firstName.endsWith("e"))
                     );
                 },
                 () -> {
-                    // Grouped assertion, so processed independently
-                    // of results of first name assertions.
+                    // 그룹화된 assertion, 이전의 이름 assertion과 독립적으로 처리된다.
                     String lastName = person.getLastName();
                     assertNotNull(lastName);
 
-                    // Executed only if the previous assertion is valid.
+                    // assertNotNull(lastName);이 검증됐을 경우에만 실행됨
                     assertAll("last name",
                             () -> assertTrue(lastName.startsWith("D")),
                             () -> assertTrue(lastName.endsWith("e"))
@@ -80,30 +77,31 @@ class AssertionsDemoTest {
     void timeoutNotExceeded() {
         // The following assertion succeeds.
         assertTimeout(ofMinutes(2), () -> {
-            // Perform task that takes less than 2 minutes.
+            // 2분내로 테스트가 종료되므로 성공
         });
     }
 
     @Test
     void timeoutNotExceededWithResult() {
-        // The following assertion succeeds, and returns the supplied object.
+        // 테스트가 성공하면 String 타입의 결과 반환
         String actualResult = assertTimeout(ofMinutes(2), () -> {
             return "a result";
         });
+        // 시간초과 테스트가 성공했는지 검증
         assertEquals("a result", actualResult);
     }
 
     @Test
     void timeoutNotExceededWithMethod() {
-        // The following assertion invokes a method reference and returns an object.
+        // AssertionsDemoTest 클래스의 메소드 참조를 호출하고 객체를 반환받는다.
         String actualGreeting = assertTimeout(ofMinutes(2), AssertionsDemoTest::greeting);
+        // 반환받은 객체(메시지) 검증
         assertEquals("Hello, World!", actualGreeting);
     }
 
     @Test
     void timeoutExceeded() {
-        // The following assertion fails with an error message similar to:
-        // execution exceeded timeout of 10 ms by 91 ms
+        // 10밀세컨 뒤에 타임아웃이 발생하지만 람다 내부에서 실행하는 본문은 100밀세컨을 기다리므로 테스트 실패
         assertTimeout(ofMillis(10), () -> {
             // Simulate task that takes more than 10 ms.
             Thread.sleep(100);
@@ -112,14 +110,14 @@ class AssertionsDemoTest {
 
     @Test
     void timeoutExceededWithPreemptiveTermination() {
-        // The following assertion fails with an error message similar to:
-        // execution timed out after 10 ms
+        // 위와 동일한 이유로(시간 초과) 테스트 실패
         assertTimeoutPreemptively(ofMillis(10), () -> {
             // Simulate task that takes more than 10 ms.
             new CountDownLatch(1).await();
         });
     }
 
+    // 테스트서 사용되는 메소드
     private static String greeting() {
         return "Hello, World!";
     }
