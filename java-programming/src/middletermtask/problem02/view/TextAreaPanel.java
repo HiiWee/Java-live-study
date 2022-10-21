ea21cd5ce52a7c5024d7c1f3401c5408565908ec
HiiWee <wpdlzhf159@gmail.com>
2022-10-21 20:08:54 +0900

    Add: TextArea용 커스텀 패널 구현

---
+++ java-programming/src/middletermtask/problem02/view/TextAreaPanel.java
@@ -0,0 +1,22 @@
+package middletermtask.problem02.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextAreaPanel extends JPanel {
+    private JLabel subjectLabel;
+    private JTextArea inputArea;
+
+    private TextAreaPanel(JLabel subjectLabel, JTextArea inputArea) {
+        this.subjectLabel = subjectLabel;
+        this.inputArea = inputArea;
+    }
+
+    public TextAreaPanel(String subject, int rows, int columns) {
+        this(new JLabel(subject), new JTextArea(rows, columns));
+        this.setLayout(new GridLayout(2, 1));
+        this.add(subjectLabel);
+        this.add(inputArea);
+    }
+
+}
