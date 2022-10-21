0773e3cc84bdb938a716810ad43aff71b39530db
HiiWee <wpdlzhf159@gmail.com>
2022-10-21 23:11:18 +0900

    Add: 유틸 메시지 추가

---
+++ java-programming/src/middletermtask/problem02/utils/UtilMessages.java
@@ -0,0 +1,12 @@
+package middletermtask.problem02.utils;
+
+public interface UtilMessages {
+    // Exception
+    String DUPLICATED_PERSON = "이미 등록된 사람입니다.";
+    String NO_PERSON_NAME = "이름은 필수로 작성해야 합니다.";
+    String NO_PERSON = "존재하지 않는 사람 입니다.";
+    String HAS_ERROR = "HAS_ERROR";
+
+    // Normal
+    String SAVE_OK = "저장완료";
+}
