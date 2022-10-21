5d62049d3382b275da473d0f719fb2d169ba1ca1
HiiWee <wpdlzhf159@gmail.com>
2022-10-21 20:08:19 +0900

    Add: TextField용 커스텀 패널 구현

---
+++ java-programming/src/middletermtask/problem02/view/TextFieldPanel.java
@@ -0,0 +1,21 @@
+package middletermtask.problem02.view;
+
+import javax.swing.*;
+
+public class TextFieldPanel extends JPanel {
+    private JLabel subjectLabel;
+    private JTextField inputField;
+
+    // 임의 호출 방지
+    private TextFieldPanel(JLabel subjectLabel, JTextField inputField) {
+        this.subjectLabel = subjectLabel;
+        this.inputField = inputField;
+    }
+    public TextFieldPanel(String subject, int columns) {
+        this(new JLabel(subject), new JTextField(columns));
+
+        this.add(subjectLabel);
+        this.add(inputField);
+    }
+
+}
