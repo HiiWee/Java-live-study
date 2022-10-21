221451e05f235b1ef4f8463ce1d731e2bf04e0ed
HiiWee <wpdlzhf159@gmail.com>
2022-10-21 22:09:30 +0900

    Refact: Person 도메인 setter 제거하고, 생성자로 초기화 하게끔 변경

--- java-programming/src/middletermtask/problem02/domain/Person.java
+++ java-programming/src/middletermtask/problem02/domain/Person.java
@@ -5,27 +5,21 @@ public class Person {
     private String phone;
     private String address;
 
-    public String getName() {
-        return name;
+    public Person(String name, String phone, String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getName() {
+        return name;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 }
