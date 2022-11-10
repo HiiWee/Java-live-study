96e1346169dda679fc5a43a0fe63872e06497d21
HiiWee <wpdlzhf159@gmail.com>
2022-11-11 04:14:09 +0900

    Test: Set.contains() 존재하면 true 존재하지 않으면 false 반환 테스트

--- core/src/test/java/SetTest.java
+++ core/src/test/java/SetTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class SetTest {
@@ -34,7 +35,6 @@ class SetTest {
     }
 
     // JUnit의 ParameterizedTest를 활용해 1, 2, 3에 대한 Set.contains() 메소드를 테스트하자
-
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 3})
     @DisplayName("Set.contains() 메소드를 테스트")
@@ -43,5 +43,15 @@ class SetTest {
         // when
         // then
         assertTrue(numbers.contains(number));
+    }
+
+    // 1,2,3의 값은 contains 메소드 실행결과 true, 4, 5값은 false가 반환되는 테스트를 구성
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains_set_내부에_존재하거나_존재하지_않는_데이터확인(int input, boolean expected) {
+        // given
+        // when
+        // then
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
