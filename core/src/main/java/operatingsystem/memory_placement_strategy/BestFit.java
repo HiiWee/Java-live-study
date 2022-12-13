8e3efdbb46a1696f358b2e6f7d3df63df5de3b29
HiiWee <wpdlzhf159@gmail.com>
2022-12-13 19:57:17 +0900

    fix: 최적 적합 기법 알고리즘 패키지 변경

--- core/src/main/java/operatingsystem/memory_placement_strategy/BestFit.java
+++ core/src/main/java/operatingsystem/memory_placement_strategy/BestFit.java
@@ -1,44 +1,44 @@
-package operatingsystem;
+package operatingsystem.memory_placement_strategy;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-class Application {
+class BestFitApplication {
     public static void main(String[] args) {
-        List<Block> blocks = new ArrayList<>();
-        blocks.add(new Block(17000, false, 17000));
+        List<BestFitBlock> blocks = new ArrayList<>();
+        blocks.add(new BestFitBlock(17000, false, 17000));
         // 이미 사용중인 공간을 나타냅니다.
-        blocks.add(new Block(1000, true, 0));
-        blocks.add(new Block(7000, false, 7000));
+        blocks.add(new BestFitBlock(1000, true, 0));
+        blocks.add(new BestFitBlock(7000, false, 7000));
         // 이미 사용중인 공간을 나타냅니다.
-        blocks.add(new Block(1000, true, 0));
-        blocks.add(new Block(15000, false, 15000));
+        blocks.add(new BestFitBlock(1000, true, 0));
+        blocks.add(new BestFitBlock(15000, false, 15000));
         // 이미 사용중인 공간을 나타냅니다.
-        blocks.add(new Block(1000, true, 0));
-        blocks.add(new Block(30000, false, 30000));
+        blocks.add(new BestFitBlock(1000, true, 0));
+        blocks.add(new BestFitBlock(30000, false, 30000));
 
-        Memory mainMemory = new Memory(blocks);
-        List<Block> emptyBlocks = mainMemory.findEmptyBlocks();
+        BestFitMemory mainMemory = new BestFitMemory(blocks);
+        List<BestFitBlock> emptyBlocks = mainMemory.findEmptyBlocks();
         List<Integer> memoriesToAllocate = List.of(12000, 5000, 30000);
 
         BestFit bestFit = new BestFit(emptyBlocks, memoriesToAllocate);
-        bestFit.AllocateMemory();
+        bestFit.allocateMemory();
         mainMemory.printMemory();
     }
 }
 
 public class BestFit {
-    private final List<Block> emptyBlocks;
+    private final List<BestFitBlock> emptyBlocks;
     private final List<Integer> memoriesToAllocate;
 
-    public BestFit(List<Block> emptyBlocks, List<Integer> memoriesToAllocate) {
-        emptyBlocks.sort(Comparator.comparingInt(Block::getSize));
+    public BestFit(List<BestFitBlock> emptyBlocks, List<Integer> memoriesToAllocate) {
+        emptyBlocks.sort(Comparator.comparingInt(BestFitBlock::getSize));
         this.emptyBlocks = emptyBlocks;
         this.memoriesToAllocate = memoriesToAllocate;
     }
 
-    public void AllocateMemory() {
+    public void allocateMemory() {
         for (int memoryToAllocate : memoriesToAllocate) {
             emptyBlocks.stream()
                     .filter(block -> block.getSize() >= memoryToAllocate && block.isNotOccupied())
@@ -49,31 +49,31 @@ public class BestFit {
     }
 }
 
-class Memory {
+class BestFitMemory {
     public static final String MESSAGE =
             "블록 전체사이즈\t\t\t\t" + "할당여부\t\t\t\t" + "남은 사이즈\t\t\t\t" + "블록 시작 주소\t\t\t\t" + "블록 마지막 주소";
     public static final int INDEX_GAP = 1;
-    private final List<Block> blocks;
+    private final List<BestFitBlock> blocks;
 
-    public Memory(List<Block> blocks) {
+    public BestFitMemory(List<BestFitBlock> blocks) {
         this.blocks = blocks;
         initBlockIndex();
     }
 
     private void initBlockIndex() {
-        Block previousBlock = blocks.get(0);
+        BestFitBlock previousBlock = blocks.get(0);
         previousBlock.setStartIndex(0);
 
         for (int blockIndex = 1; blockIndex < blocks.size(); blockIndex++) {
-            Block currentBlock = blocks.get(blockIndex);
+            BestFitBlock currentBlock = blocks.get(blockIndex);
             currentBlock.setStartIndex(previousBlock.getEndIndex() + INDEX_GAP);
             previousBlock = currentBlock;
         }
     }
 
-    public List<Block> findEmptyBlocks() {
-        List<Block> emptyBlocks = new ArrayList<>();
-        for (Block block : blocks) {
+    public List<BestFitBlock> findEmptyBlocks() {
+        List<BestFitBlock> emptyBlocks = new ArrayList<>();
+        for (BestFitBlock block : blocks) {
             if (block.isNotOccupied()) {
                 emptyBlocks.add(block);
             }
@@ -87,7 +87,7 @@ class Memory {
     }
 }
 
-class Block {
+class BestFitBlock {
     public static final int INDEX_GAP = 1;
 
     private final int size;
@@ -96,7 +96,7 @@ class Block {
     private int startIndex;
     private int endIndex;
 
-    public Block(int size, boolean isOccupied, int leftSize) {
+    public BestFitBlock(int size, boolean isOccupied, int leftSize) {
         this.size = size;
         this.isOccupied = isOccupied;
         this.leftSize = leftSize;
