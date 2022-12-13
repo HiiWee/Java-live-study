3f30eb51d4492f409a4449bdf21f7fcb84b6927f
HiiWee <wpdlzhf159@gmail.com>
2022-12-14 05:13:51 +0900

    add: c-scan 알고리즘 추가

---
+++ core/src/main/java/operatingsystem/disk_scheduling/cscan/CScan.java
@@ -0,0 +1,165 @@
+package operatingsystem.disk_scheduling.cscan;
+
+import static operatingsystem.disk_scheduling.scan.ScanAlgorithm.MAX_TRACK_INDEX;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+class RandomGenerator {
+    private RandomGenerator() {
+    }
+
+    public static int getRandomNumberInRange(int start, int end) {
+        Random random = new Random();
+        return random.nextInt(end - start) + start;
+    }
+}
+
+public class CScan extends Thread {
+    public static final int MAX_TRACK_INDEX = 199;
+    private final List<Integer> left = new ArrayList<>();
+    private final PriorityQueue<Integer> right = new PriorityQueue<>();
+    private int headValue;
+    private String direction;
+    private int distance;
+
+    public CScan(final int headValue, String direction) {
+        this.headValue = headValue;
+        this.direction = direction;
+    }
+
+    @Override
+    public void run() {
+        try {
+            startScan();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void startScan() throws InterruptedException {
+        startRightScan();
+    }
+
+    private void startLeftScan() throws InterruptedException {
+        int previewHeadValue;
+        int leftSize = left.size();
+        Collections.sort(left);
+        while (leftSize-- > 0) {
+            previewHeadValue = headValue;
+            if (!right.isEmpty() && left.get(0) > right.peek()) {
+                headValue = right.poll();
+                distance += Math.abs(headValue - previewHeadValue);
+                printMovingStatus(previewHeadValue);
+                Thread.sleep(400);
+                continue;
+            }
+            headValue = left.get(0);
+            left.remove(0);
+            distance += Math.abs(headValue - previewHeadValue);
+            printMovingStatus(previewHeadValue);
+            Thread.sleep(400);
+        }
+        if (!right.isEmpty()) {
+            System.out.println("\n change\n");
+            startRightScan();
+        }
+    }
+
+
+    private void startRightScan() throws InterruptedException {
+        int previewHeadValue;
+        while (!right.isEmpty()) {
+            previewHeadValue = headValue;
+            headValue = right.poll();
+            right.remove(0);
+            distance += Math.abs(headValue - previewHeadValue);
+            printMovingStatus(previewHeadValue);
+            Thread.sleep(400);
+        }
+        distance += Math.abs(MAX_TRACK_INDEX - headValue);
+        System.out.println(headValue + " -> " + MAX_TRACK_INDEX + " = " + distance);
+        headValue = MAX_TRACK_INDEX;
+        if (!left.isEmpty()) {
+            distance += 199;
+            System.out.println("\nback to start " + MAX_TRACK_INDEX + " -> " + 0 + "\n");
+            headValue = 0;
+            startLeftScan();
+        }
+    }
+
+    private void printMovingStatus(final int previewHeadValue) {
+        System.out.print(previewHeadValue + " -> " + headValue);
+        System.out.println(" = " + distance);
+    }
+
+    public void initializeValue(final List<Integer> input) {
+        for (int value : input) {
+            if (headValue < value) {
+                right.add(value);
+                continue;
+            }
+            left.add(value);
+        }
+    }
+
+    public int getResult() {
+        return distance;
+    }
+
+    public void addValue(final int randomTrackValue) {
+        System.out.println("input random value: " + randomTrackValue);
+        if (headValue < randomTrackValue) {
+//            System.out.println("pass");
+            right.add(randomTrackValue);
+            return;
+        }
+        left.add(randomTrackValue);
+    }
+}
+
+class RandomTrackThread extends Thread {
+    private int time;
+    private CScan CScan;
+
+    public RandomTrackThread(final int time, final CScan CScan) {
+        this.time = time;
+        this.CScan = CScan;
+    }
+
+    @Override
+    public void run() {
+        while (time-- > 0) {
+            int randomBreakTime = RandomGenerator.getRandomNumberInRange(1, 1000);
+            int randomTrackValue = RandomGenerator.getRandomNumberInRange(0, MAX_TRACK_INDEX);
+            try {
+                Thread.sleep(randomBreakTime);
+                CScan.addValue(randomTrackValue);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Application {
+    public static final String RIGHT = "right";
+
+    public static void main(String[] args) throws InterruptedException {
+        CScan CScan = new CScan(50, RIGHT);
+        RandomTrackThread randomTrackThread = new RandomTrackThread(10, CScan);
+        List<Integer> input = List.of(105, 180, 40, 120, 10, 125, 65, 70);
+        CScan.initializeValue(input);
+
+        CScan.start();
+        randomTrackThread.start();
+
+        CScan.join();
+        randomTrackThread.join();
+        System.out.println("결과: " + CScan.getResult());
+
+    }
+}
